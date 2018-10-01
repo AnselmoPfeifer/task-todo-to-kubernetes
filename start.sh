4b4cc3c4196235c374608d84ce643477499d69a9
Anselmo Pfeifer <apfeifer@daitangroup.com>
2018-10-01 11:27:14 -0400

    Added check on start script

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+ACTION=${1}
 
-(cd terraform && terraform destroy -auto-approve)
-#(cd terraform && terraform init; terraform plan; terraform apply -auto-approve)
+if [ ${ACTION} == 'create' ]; then
+    echo "Creating new k8s cluster using terraform apply"
+    (cd terraform && terraform init; terraform plan; terraform apply -auto-approve)
+
+elif [ ${ACTION} == 'destroy' ]; then
+    echo "Destroying k8s cluster using terraform destroy"
+    (cd terraform && terraform destroy -auto-approve)
+else
+    echo "You have to use: create or destroy"
+    exit 1
+fi
